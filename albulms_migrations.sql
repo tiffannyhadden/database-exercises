2fed8261ce49abde63a13d537ecc2a6b53367fd2
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-27 11:45:25 -0500

    adds album exercise

--- albulms_migrations.sql
+++ albulms_migrations.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) NOT NULL,
                         name VARCHAR(100) NOT NULL,
-                        release_date DATE NOT NULL,
+                        release_date INT NOT NULL,
                         sales FLOAT,
                         genre VARCHAR(50),
                         PRIMARY KEY (id)
---
+++ albums_cedar.sql
@@ -0,0 +1,5 @@
+codeup_test_db;
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ('Douglas', 'Adams',    'Time is an illusion. Lunchtime doubly so.'),
+       ('Mark',    'Twain',    'Clothes make the man. Naked people have little or no influence on society.'),
+       ('Kurt',    'Vonnegut', 'The universe is a big place, perhaps the biggest.');
