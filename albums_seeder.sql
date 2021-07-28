1785364d4a08e8ad8446da92e82a630f1c5af8a7
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-28 09:32:58 -0500

    adds albums seeder

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+codeup_test_db;
+INSERT INTO albums (artist, Name, release_date, sales, genre)
+VALUES ('Micheal Jackson','Thriller', 1982, '47.9','Pop, post-disco, funk, rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, '32.4','R&B, soul, pop, soundtrack'),
+       ('Eagles','Their Greatest Hits (1971-1975)',1976,'41.2','Country rock, soft rock, folk rock');
+
+SELECT * FROM albums;
