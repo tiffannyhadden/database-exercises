a12bd3d91430ab77623d6bf03fd373666bc3e69e
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-29 09:51:05 -0500

    adds order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
+
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
